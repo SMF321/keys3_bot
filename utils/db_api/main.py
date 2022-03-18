ff6741cd9964ec2ce28907b12f0a2e0de645709d
Jastebsz <jastebsz@gmail.com>
2022-03-19 00:42:31 +0300

    16

--- utils/db_api/main.py
+++ utils/db_api/main.py
@@ -59,6 +59,7 @@ def POST_QUESTION(id1, question1,  datetime1):
 def POST_CLASS_QUESTION(id1, class_question1):
     query = db.update(Question).values(Class_question=class_question1)
     query = query.where(Question.columns.Id == id1)
+
     results = connection.execute(query)
 
 
