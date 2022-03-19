78d5a0437f046992151d43fae62ea747cf68bc5a
SMF321 <tokmakov852456@gmail.com>
2022-03-19 13:07:56 +0300

    45

--- handlers/users/admin_tree.py
+++ handlers/users/admin_tree.py
@@ -5,17 +5,18 @@ from loader import dp
 
 from states.bot_states import Register
 from keyboards.default.bot_button import add_button, admin_menu_button, topics_for_appeals
+from utils.db_api.main import *
 
 
 @dp.message_handler(state=Register.admin_start, content_types=types.ContentTypes.ANY)
 async def bot_echo_all(message: types.Message, state: FSMContext):
     if message.text == admin_menu_button[0]:
-        await message.answer(f"Выберите тему по которой хотите просмотреть обращения", reply_markup=add_button(topics_for_appeals))
+        await message.answer(f"Выберите тему по которой хотите просмотреть обращения", reply_markup=add_button(GET_SUGGESTIONS()))
         # TODO сделаю потом, но тут kolya
         await Register.admin_start.set()
         await message.answer(f"Меню:", reply_markup=add_button(admin_menu_button))
     elif message.text == admin_menu_button[1]:
-        await message.answer(f"Выберите тему по которой хотите создать группу для обсуждения", reply_markup=add_button(topics_for_appeals))
+        await message.answer(f"Введите название темы по которую хотите добавить:")
         await Register.created_chat.set()
         # await message.answer(f"Меню:", reply_markup=add_button(user_menu_button))
     elif message.text == admin_menu_button[2]:
@@ -26,6 +27,4 @@ async def bot_echo_all(message: types.Message, state: FSMContext):
 
 @dp.message_handler(state=Register.created_chat, content_types=types.ContentTypes.ANY)
 async def bot_echo_all(message: types.Message, state: FSMContext):
-    await message.answer(f"тут ебать муть будет но ща придумаем")
-    # TODO kolya
-    await message.answer(f"Меню:", reply_markup=add_button(admin_menu_button))
+    POST_NEW_SUGGESTIONS()
